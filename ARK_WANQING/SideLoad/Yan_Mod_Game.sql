59b67a94d807771d5a4c5f3e35f82141cb8143e2
HaoJun0823 <developer@haojun0823.xyz>
2026-02-06 14:23:02 +0800

    Add YanMod Support.

---
+++ ARK_WANQING/SideLoad/Yan_Mod_Game.sql
@@ -0,0 +1,16 @@
+-- Yan_Mod_Game
+-- Author: HaoJun0823
+--------------------------------------------------------------
+
+INSERT OR IGNORE INTO CivilizationLeaders 
+(
+    CivilizationType, 
+    LeaderType, 
+    CapitalName
+)
+VALUES
+(
+    'CIVILIZATION_OP_YAN', 
+    'LEADER_RANDERION_ARK_WANQING_RED', 
+    'LOC_CITY_NAME_ARK_WANQING_REAL_CAPITAL'
+);
