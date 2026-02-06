3882f198623a3a3b2ec9304a88b0692e33cffc9f
HaoJun0823 <developer@haojun0823.xyz>
2026-02-06 14:47:54 +0800

    fix optional yan mod support.

--- ARK_WANQING/SideLoad/Yan_Mod_Game.sql
+++ ARK_WANQING/SideLoad/Yan_Mod_Game.sql
@@ -2,15 +2,15 @@
 -- Author: HaoJun0823
 --------------------------------------------------------------
 
+-- 建立文明与领袖的关联（带存在性检测）
 INSERT OR IGNORE INTO CivilizationLeaders 
 (
     CivilizationType, 
     LeaderType, 
     CapitalName
 )
-VALUES
-(
+SELECT 
     'CIVILIZATION_OP_YAN', 
     'LEADER_RANDERION_ARK_WANQING_RED', 
     'LOC_CITY_NAME_ARK_WANQING_REAL_CAPITAL'
-);
+WHERE EXISTS (SELECT 1 FROM Civilizations WHERE CivilizationType = 'CIVILIZATION_OP_YAN');
